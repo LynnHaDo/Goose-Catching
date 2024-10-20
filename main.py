3884c9a2c9cabac95bb2181d39ab9148d4765f9c
Lynn Do <do24l@mtholyoke.edu>
2024-10-20 18:00:06 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from goose import Goose 
+
+# Import mediapipe modules
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+# URL to the model
+MODEL_PATH = 'https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/latest/hand_landmarker.task'
+
+
+
